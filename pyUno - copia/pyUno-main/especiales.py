cb00d7b407131645001d7ecca0b9fc22b17d0eba
numbasan-san <80220352+numbasan-san@users.noreply.github.com>
2022-12-12 20:59:49 -0400

    Modificación
    - Corrección de errores menores.

--- pyUno - copia/pyUno-main/especiales.py
+++ pyUno - copia/pyUno-main/especiales.py
@@ -6,20 +6,15 @@ class Especiales:
 
 	def __init__(self):
 		self.none = None
-    
-	def get_index(jugador):
+
+	@staticmethod
+	def skill_plus(jugador, mesa, plus, sentido):
+
 		index = jugadores.index(jugador)
 		if index == len(jugadores) - 1:
 			index =  0
 		else:
 			index = index + 1
-			
-		return index
-		
-	@staticmethod
-	def skill_plus(self, jugador, mesa, plus):
-
-		index = self.get_index(jugador)
 
 		for i in range(plus):
 			mesa.mezclar()
@@ -38,13 +33,17 @@ class Especiales:
 				return palos[palo]
 
 	@staticmethod
-	def skill_ginyu(self, jugador):
+	def skill_ginyu(jugador):
 
-		index = self.get_index(jugador)
+		index = jugadores.index(jugador)
+		if index == len(jugadores) - 1:
+			index =  0
+		else:
+			index = index + 1
 
 		mano_1 = jugador.mano
 		mano_2 = jugadores[index].mano
 		jugador.mano = mano_2
 		jugadores[index].mano = mano_1
 		print('VEYETTA #########!!!!!')
-		
+#

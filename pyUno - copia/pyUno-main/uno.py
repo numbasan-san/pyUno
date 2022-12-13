59155ac2a6eae9d5594c453ef196696b172864c5
numbasan-san <80220352+numbasan-san@users.noreply.github.com>
2022-12-12 21:06:51 -0400

    Update uno.py

--- pyUno - copia/pyUno-main/uno.py
+++ pyUno - copia/pyUno-main/uno.py
@@ -27,24 +27,34 @@ class Uno:
                 self.mesa.mezclar()
                 jugador.tomar_carta(self.mesa.mazo[0])
 
-				
-    '''
-    BUG.
-    
-    Al momento de hacer el reverso se salta jugadores de vez en vez.
-    '''
     def ronda(self):
 
 		#print('\nCarta en Mesa:', self.mesa.cartas_mesa.etiqueta)
-
-        for i in range(len(jugadores)):
+        players = jugadores[::self.sentido]
+        # jugadores = players
+        
+        print(f'Supuesta lista inversa de los jugadores:')
+        for i in range(len(players)):
+            print(f'{players[i].nombre}')
+            
+        '''
+        i = 0
+        while i <= len(jugadores) and i >= -(len(jugadores)):
             print('\nCarta en Mesa:', self.mesa.cartas_mesa.etiqueta)
-            #print('Logitud de la mano del jugador:', len(jugador.mano))
-            null = 0 if self.sentido == 1 else 1
-            index = i + ( null * ( self.sentido * ( len(jugadores) ) ) ) 
-            # print(f'Supuesto inverso: {index}')
-            self.jugar(jugadores[ index ]) # self.sentido
-
+            self.jugar(players[ i ])
+            i += self.sentido
+            if i >= len(jugadores):
+               i = 0
+            if i <= -(len(jugadores)):
+               i =  - 1
+        '''
+        for i in range(len(players)):
+            print('\nCarta en Mesa:', self.mesa.cartas_mesa.etiqueta)
+            # print('Logitud de la mano del jugador:', len(jugador.mano))
+            '''null = 0 if self.sentido == 1 else 1'''
+            index = i * self.sentido 
+            print(f'Supuesto inverso: {index}')
+            self.jugar(players[ index ]) # self.sentido
 		#print('\nPrimera carta:', self.mesa.mostrar())
 		# print(self.mesa.mostrar_todo())
 
@@ -52,48 +62,62 @@ class Uno:
         print('\nMano de', jugador.nombre)
         jugador.imprimir_mano(selection = True)
         print(len(jugadores))
-        action = Utilities.opciones("¿Qué harás? Ver, Tirar, Robar. [V, T, R] ", ['V', 'T', 'R'])
 
-        if action == 'T':
-            selection = (Utilities.pregunta('¿Cuál carta tirará? ', 1, len(jugador.mano), -1))
-            valid_cards = self.valid_cards(jugador, jugador.mano)
-            action = Utilities.validar(selection, valid_cards)
+        valid_cards = self.valid_cards(jugador, jugador.mano)
+        print(f'Cartas válidas: {valid_cards}.')
 
-            if  jugador.mano[selection - 1] in valid_cards:
-                if jugador.mano[selection - 1].valor == 'Joker':
-# BUG.
-# Cambia el color/palo de las demás cartas Joker del juego al cambiar el palo del joker lanzado.
-                    jugador.mano[selection - 1].palo = self.especiales.skill_joker()
-                    jugador.mano[selection - 1].etiqueta = jugador.mano[selection].valor + ' ' + jugador.mano[selection].palo
-                    self.mesa.servir_mesa(jugador.mano.pop(selection - 1))
-    
-                elif jugador.mano[selection - 1].valor == 'Ginyu':
-                    self.mesa.servir_mesa(jugador.mano.pop(selection - 1))
+        if len(valid_cards) >= 1:
+
+            action = Utilities.opciones("¿Qué harás? Ver, Tirar, Robar. [V, T, R] ", ['V', 'T', 'R'])
+
+            if action == 'T':
+                selection = (Utilities.pregunta('¿Cuál carta tirará? ', 1, len(jugador.mano), -1))
+                # print(f'Uno.jugar.selection: {selection}')
+                select_valid = Utilities.validar(selection, valid_cards)
+                selection = int(select_valid) - 1
+
+                print(f'Carta lanzada: {jugador.mano[selection].etiqueta}. Indice: {str(selection)}')
+
+                if jugador.mano[selection].valor == 'Joker':
+                    # BUG.
+                    # Cambia el color/palo de las demás cartas Joker del juego al cambiar el palo del joker lanzado.
+                    jugador.mano[selection].palo = self.especiales.skill_joker()
+                    jugador.mano[selection].etiqueta = jugador.mano[selection].valor + ' ' + jugador.mano[selection].palo
+                    self.mesa.servir_mesa(jugador.mano.pop(selection))
+
+                elif jugador.mano[selection].valor == 'Ginyu':
+                    self.mesa.servir_mesa(jugador.mano.pop(selection))
                     self.especiales.skill_ginyu(jugador)
-    
-                elif jugador.mano[selection - 1].valor == '+4':
-                    plus = 4
-                    self.especiales.skill_plus(jugador, self.mesa, plus)
-                    self.mesa.servir_mesa(jugador.mano.pop(selection - 1))
-			
-                elif jugador.mano[selection - 1].valor == '+2':
-                    plus = 2 
-                    self.especiales.skill_plus(jugador, self.mesa, plus)
-                    self.mesa.servir_mesa(jugador.mano.pop(selection - 1))
-                else:
-                    self.mesa.servir_mesa(jugador.mano.pop(selection - 1))
-            else:
-                print(f'Carta inválida. Elija otra.')
-                
 
-        elif action == 'R':
+                elif jugador.mano[selection].valor == '+4' or jugador.mano[selection].valor == '+2':
+                    plus = 4 if jugador.mano[selection].valor == '+4' else 2
+                    self.especiales.skill_plus(jugador, self.mesa, plus, self.sentido)
+                    self.mesa.servir_mesa(jugador.mano.pop(selection))
+
+                elif jugador.mano[selection].valor == 'Reverso':
+                    # BUG.
+                    # Al momento de hacer el reverso se salta jugadores de vez en vez.
+                    self.sentido *= -1
+                    self.mesa.servir_mesa(jugador.mano.pop(selection))
+                    print(f'Kira Kuin Dai San no Bakkudan: Baito Za Dasuto!')
+
+                elif not jugador.mano[selection].valor in ['+2', '+4', 'Ginyu', 'Joker']:
+                    self.mesa.servir_mesa(jugador.mano.pop(selection))
+
+            elif action == 'R':
+                self.mesa.mezclar()
+                jugador.tomar_carta(self.mesa.mazo[0])
+
+        else:
+            print(f'Cartas insuficientes para jugar. Robo atumático.')
             self.mesa.mezclar()
             jugador.tomar_carta(self.mesa.mazo[0])
 
     def valid_cards(self, jugador, mano):
         valid_cards = []
         for i in range(len(mano)):
-            if self.mesa.cartas_mesa.valor in ['Joker', 'Ginyu', '+4'] or \
+            if self.mesa.cartas_mesa.valor in ['Ginyu', '+4'] or \
+            jugador.mano[i].valor in ['Joker', 'Ginyu', '+4'] or \
             ((jugador.mano[i].valor == self.mesa.cartas_mesa.valor) or \
             (jugador.mano[i].palo == self.mesa.cartas_mesa.palo)):
                 valid_cards.append(str(i + 1))
@@ -103,3 +127,4 @@ class Uno:
         opt = Utilities.opciones("¿Terminar? ['Y', 'N'] ", ['Y', 'N'])
         end = True if opt == 'Y' else False
         return end
+#
